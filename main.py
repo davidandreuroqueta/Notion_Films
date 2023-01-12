fda5518475981fc232ec9a1549f09293ada560e5
davidandreuroqueta <david.andreu.roqueta@gmail.com>
2023-01-12 19:35:29 +0100

    Terminal interface finished

--- main.py
+++ main.py
@@ -1,29 +1,83 @@
 from peliculas import Film
 from notion import NotionClient
 import os
+import json
 from dotenv import load_dotenv
 from csv import DictReader
-
-load_dotenv()
-
-notion_token = os.getenv('NOTION_TOKEN')
-tmdb_key = os.getenv('TMDB_TOKEN')
-language = 'es-ES' #ISO 639-1
-region = 'ES' #https://developers.themoviedb.org/3/movies/get-movie-watch-providers
-
-databaseId = os.getenv('DATABASEID')
-
-
-headers = {
-    "Accept": "application/json",
-    "Notion-Version": "2022-02-22",
-    "Authorization": f"Bearer {notion_token}"
-}
 
 # def new_filmpage(client, name):
     # film = Film(name)
     # res = client.create_page(film)
     # return res
+
+def get_settings():   
+    with open('./settings.json', 'r') as f:
+        data = json.load(f)
+        return data['settings'].values()
+    
+def set_settings(name, value):
+    with open('./settings.json', 'r+') as f:
+        data = json.load(f)
+        data['settings'][name] = value
+        f.seek(0)
+        json.dump(data, f)
+        f.truncate()
+    
+def ask_for(ini_quest, expl, instance, l = []):
+    res = None
+    print(l)
+    print(len(l)==0, res in l)
+    while not (len(l) == 0 or res in l):
+        try:
+            res = instance(input(ini_quest))
+        except:
+            print('Invalid answare. The response datatype is not correct.')
+            continue
+        if not (len(l) == 0 or res in l):
+            print(expl)
+    return instance(res)
+
+def first_page(notion_token, databaseId, tmdb_key, language, region):
+    print('#'*50 + '\n' +
+          '#'*5 + f'{"NOTION_FILMS APP":^40}' + '#'*5 + '\n' +
+          '#'*50 + '\n')
+    
+    first = ask_for('Do you want to change the settings (y/n): ', 'Response not valid. Answer "y", "yes", "n" or "no". ', str, ['n', 'no', 'y', 'yes'])
+
+    if first.lower() in ('y', 'yes'):
+        return second_page(notion_token, databaseId, tmdb_key, language, region)
+    
+def second_page(notion_token, databaseId, tmdb_key, language, region):
+    print('The current options are: ' + '\n' + '\n' +
+        'Notion token: ' + notion_token + '\n' +
+        'Notion database ID: ' + databaseId + '\n' +
+        'TMDB key: ' + tmdb_key + '\n' +
+        'Language (ISO 639-1): ' + language + '\n' +
+        'Region (Look for it in the TMDB doc): ' + region + '\n'+ '\n'+ '\n')
+    
+    print('Change:' + '\n' +
+            '1) Notion token' + '\n' +
+            '2) Notion database ID' + '\n' +
+            '3) TMDB key' + '\n' +
+            '4) Language' + '\n' +
+            '5) Region' + '\n' +
+            '6) GO BACK')
+    
+    dic_options = {k+1: v for k, v in enumerate(['notion_token', 'databaseId', 'tmdb_key', 'language', 'region'])}
+    print(dic_options)
+    
+    option = ask_for('Choose an option: ', 'Option not valid. Must be a number between 1-6', int, [1,2,3,4,5,6])
+    if option == 6:
+        return first_page(notion_token, databaseId, tmdb_key, language, region)
+    new_value = input('New value: ')
+    sure = ask_for('Are you sure? (y/n)', 'Response not valid. Answer "y", "yes", "n" or "no". ', str, ['n', 'no', 'y', 'yes'])
+    if sure in {'y', 'yes'}:
+        set_settings(dic_options[option], new_value)
+    else:
+        second_page(notion_token, databaseId, tmdb_key, language, region)
+    
+    
+    
 
 def extract_films_list(client):
     db = client.read_database()
@@ -71,22 +125,44 @@ def update_filmpage(client, tmdb_key, film_dic, region, language):
    
 
 if __name__ == '__main__':
-    client = NotionClient(notion_token, databaseId)
-    with open("films_info.csv", 'r') as f:
-     
-        dict_reader = DictReader(f)
-        
-        films_info = list(dict_reader)
+    load_dotenv()
+
+    # Read settings from json
+    notion_token, databaseId, tmdb_key, language, region = get_settings()
+
+    notion_token = os.getenv('NOTION_TOKEN')
+    tmdb_key = os.getenv('TMDB_TOKEN')
+    language = 'es-ES' #ISO 639-1
+    region = 'ES' #https://developers.themoviedb.org/3/movies/get-movie-watch-providers
+    databaseId = os.getenv('DATABASEID')
+
+    headers = {
+        "Accept": "application/json",
+        "Notion-Version": "2022-02-22",
+        "Authorization": f"Bearer {notion_token}"
+    }
     
-    # print(films_info)
+    first_page(notion_token, databaseId, tmdb_key, language, region)
 
-    # films_info = extract_films_list(client)
+        
+    
+    
+    # client = NotionClient(notion_token, databaseId)
+    # with open("films_info.csv", 'r') as f:
+     
+    #     dict_reader = DictReader(f)
+        
+    #     films_info = list(dict_reader)
+    
+    # # print(films_info)
 
-    if len(films_info) > 0:
-        for film_dic in films_info:
-            print(film_dic)
-            res = update_filmpage(client, tmdb_key, film_dic, region, language)
-            print(res)
-            print('\n')
-    else:
-        print('No hay películas por actualizar')
+    # # films_info = extract_films_list(client)
+
+    # if len(films_info) > 0:
+    #     for film_dic in films_info:
+    #         print(film_dic)
+    #         res = update_filmpage(client, tmdb_key, film_dic, region, language)
+    #         print(res)
+    #         print('\n')
+    # else:
+    #     print('No hay películas por actualizar')
